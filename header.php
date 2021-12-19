4ff4d79347d01e7f527bb8531257f7a129a5b766
Chakshan Kothakota <ragchak@gmail.com>
2021-12-19 15:18:51 -0800

    fixed login and profile to open in same tab

--- header.php
+++ header.php
@@ -9,12 +9,12 @@
         <?php
             if (isset($_SESSION["useruid"])) {
                 echo "<a href='includes/logout.inc.php' target='_parent'><button class='nav-button'>Log Out</button></a>";
-                echo "<a href='profile.php' target='_blank'><button class='nav-button'>Profile</button></a>";
+                echo "<a href='profile.php' target='_parent'><button class='nav-button'>Profile</button></a>";
                 echo "<a href='calendar.php' target='_parent'><button class='nav-button'>Calendar</button></a>";
             }
             else {
                 echo "<a href='signup.php' target='_parent'><button class='nav-button'>Sign Up</button></a>";
-                echo "<a href='login.php' target='_blank'><button class='nav-button'>Log In</button></a>";
+                echo "<a href='login.php' target='_parent'><button class='nav-button'>Log In</button></a>";
             }
         ?>
         
